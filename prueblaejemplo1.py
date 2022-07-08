298b07aced0c58dd18ca06b6bccb993ccfccad68
hgrande <52891798+hgrande@users.noreply.github.com>
2022-07-08 11:24:25 +0200

    Add files via upload

--- prueblaejemplo1.py
+++ prueblaejemplo1.py
@@ -5,22 +5,22 @@ from dwave.system import EmbeddingComposite, DWaveSampler
 # y sus métodos proveen utilidades para trabajar con ellos y entre las dos representaciones de un problema.
 from dimod import BinaryQuadraticModel 
 
-Q = {('B','B'): 1,('K','K'): 1,('A','C'): 2,('A','K'): -2,('B','C'): -2} #Definimos el problema como un  diccionario de Python
+Q = {('B','B'): -3,('K','K'): -3,('B','K'): -3} #Definimos el problema como un  diccionario de Python
 
 bqm = BinaryQuadraticModel.from_qubo(Q) #Lo convertimos a un BQM
 
 # Convertimos el bqm a un modelo Ising 
-ising_model = bqm.to_ising()
+#ising_model = bqm.to_ising()
 
 # Definimos el sampler que usaremos para ejecutar el problema
 sampler = EmbeddingComposite(DWaveSampler())
 
 #Ejecutamos el problema en el sampler
-sampleset = sampler.sample_ising(
-                h = ising_model[0],
-                J = ising_model[1],
-                num_reads = 10,
-                label='Example - Simple Ocean Programs: Conversion')
+#sampleset = sampler.sample_ising(
+ #               h = ising_model[0],
+  #              J = ising_model[1],
+   #             num_reads = 10,
+    #            label='Example - Simple Ocean Programs: Conversion')
 
 #ponemos el resultado en pantalla
 print(sampleset)
